81be84cbd009c8082e84b775e93562514d3622ff
Shahram-hajizaki <shahramhjz75@gmail.com>
2023-07-04 15:49:22 +0330

    created-isdon

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,20 +1,62 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { BsPlusCircleFill } from "react-icons/bs";
 import { AiFillDelete } from "react-icons/ai";
 import { FaEdit } from "react-icons/fa";
 import { IoCheckmarkDoneCircleSharp } from "react-icons/io5";
 import  "./Home.css";
+import TodoList from "../TodoList/TodoList";
 
 
 const Home = () => {
+
+    const[todoInput , setTodoInput] = useState('')
+    const[todosList , setTodosList] = useState([])
+
+
+    const handleChangeInput = (value) => {
+      setTodoInput(value)
+    }
+
+    const addTodoFunc = () => {
+        const newTodo = {
+            id: crypto.randomUUID(),
+            title: todoInput,
+            isDone: false
+        }
+        setTodosList([...todosList , newTodo])
+        setTodoInput("")
+    }
+
+    const deleteTodo = (id) => {
+        const orginalTodos = [...todosList];
+        const filtered = orginalTodos.filter((todo => todo.id !== id))
+        setTodosList(filtered)
+    }
+
+    const isDoneTodo = (id) => {
+        const orginalTodos = [...todosList];
+        const index = orginalTodos.find((todo) => todo.id === id)
+        if(index.isDone === false){
+            index.isDone = true
+        }else{
+            index.isDone = false
+        }
+        setTodosList(orginalTodos , index)
+    }
+
   return (
     <div>
         <header>
         <h1>To Do List</h1>
       </header>
       <form action="components/Home/Home">
-        <input type="text" className="todo-input" />
-        <div className="todo-button" type="submit">
+        <input
+            type="text"
+            className="todo-input"
+            onChange={(e) => handleChangeInput(e.target.value)}
+            value={todoInput}
+        />
+        <div className="todo-button" type="submit" onClick={addTodoFunc}>
           <BsPlusCircleFill className="plus-icon" /> 
         </div>
         <div className="select">
@@ -28,18 +70,17 @@ const Home = () => {
 
       <div className="todo-container">
         <ul className="todo-list">
-          <div className="todo">
-            <li className="todo-item">learn js</li>
-            <button className="complete-btn">
-              <IoCheckmarkDoneCircleSharp />
-            </button>
-            <button className="edit-btn">
-              <FaEdit />
-            </button>
-            <button className="trash-btn">
-            <AiFillDelete />
-            </button>
-          </div>
+            {todosList.map(todo => (
+                <TodoList
+                    key = {todo.id}
+                    id = {todo.id}
+                    title = {todo.title}
+                    isDone = {todo.isDone}
+                    // editFunc = {}
+                    deleteFunc = {deleteTodo}
+                    isDoneFunc = {isDoneTodo}
+                />
+            ))}
         </ul>
       </div>
     </div>
